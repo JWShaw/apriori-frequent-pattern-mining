32b39e9190a63551c3643899f60085fc88206362
Jonathan Shaw <jonathan.shaw@tuta.io>
2020-10-14 20:58:41 -0700

    commit before changing generics

--- Table.java
+++ Table.java
@@ -1,12 +1,16 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Table {
 
-    private HashMap<HashSet<Integer>, Integer> itemsets;
+    /* The rather complicated underlying data structure.  
+    Maps each itemset to itself, along with its number of occurrences. */
+    private HashMap<HashSet<Integer>, Pair<HashSet<Integer>, Integer>> itemsets;
+
     private boolean isCTable;
     private double minSupNum;   // Number of occurrences of a "frequent" item
 
@@ -19,48 +23,45 @@ public class Table {
         Scanner sc = new Scanner(f);
         int dbLines = sc.nextInt();
         this.minSupNum = dbLines * (minSupPercent / 100);
-        System.out.print(minSupNum);
 
+        // Creates and populates the L1 table
         while (sc.hasNextInt()) {
             sc.nextInt();
-            int items = sc.nextInt();
+            int numItems = sc.nextInt();
 
             /* For each item, hash a 1-element set containing that item with
             the number of times that item has occurred in the database */
-            for (int i = 0; i < items; i++) {
+            for (int i = 0; i < numItems; i++) {
+                HashSet<Integer> singleton = new HashSet<>();
+                singleton.add(sc.nextInt());
 
-                HashSet<Integer> item = new HashSet<>();
-                item.add(sc.nextInt());
-
-                if (itemsets.get(item) == null) {
-                    itemsets.put(item, 1);
+                if (itemsets.get(singleton) == null) {
+                    itemsets.put(singleton, new Pair<>(singleton, 1));
                 } else {
-                    int count = itemsets.get(item);
-                    itemsets.replace(item, ++count);
+                    // Increments the count of the set if it has already been encountered
+                    itemsets.get(singleton).second++;
                 }
             }
         }
         sc.close();
     }
 
-    // Queries the database to convert the C-table into an L-table
+    /* Used locally by the selfJoin method to build the next C-table from an 
+    L-table. */
+    // private Table(HashMap<HashSet<Integer>, Integer> underlyingHashMap) {
+    //     this.itemsets = underlyingHashMap;
+    //     this.isCTable = true;
+    // }
+    //Queries the database to convert the C-table into an L-table
+
     public void populateLTable(File f) throws FileNotFoundException {
         this.isCTable = false;
     }
 
-    /* Used locally by the selfJoin method to build the next C-table from an 
-    L-table. */
-    private Table(HashMap<HashSet<Integer>, Integer> underlyingHashMap) {
-        this.itemsets = underlyingHashMap;
-        this.isCTable = true;
-    }
-
     // Returns a new C-table based on this L-table
-    public Table selfJoin(double minsup) {
-        return this;
-
-
-    }
+    // public Table selfJoin(double minsup) {
+    //     Iterator iter1 = itemsets.iterator();
+    // }
     
     public boolean isCTable() {
         return isCTable;
